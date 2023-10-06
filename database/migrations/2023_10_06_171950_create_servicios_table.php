79e6ae42b3abca2f843c1753ebe0b997193aeb42
RosalesJhordi <1428060@senati.pe>
2023-10-06 13:09:04 -0500

    Api rest

---
+++ database/migrations/2023_10_06_171950_create_servicios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('ubicacion');
+            $table->string('clima');
+            $table->string('descripcion');
+            $table->string('horario');
+            $table->string('imagen');
+            $table->double('costo');
+            $table->double('descuento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
